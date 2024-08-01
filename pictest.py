56799d9cda07ea0f4070b0e6c77655eb1259557f
cwoolfo1 <70346478+cwoolfo1@users.noreply.github.com>
2024-07-31 20:29:53 -0400

    Major Changes! I am using built in interpolation and conjugate gradient methods that I found in Jax. I have finished writing the code for electric field PIC code. Next steps include testing for convergence and implementing magnetic field methods

--- pictest.py
+++ pictest.py
@@ -13,61 +13,26 @@ from jax import jit
 import jax.numpy as jnp
 # Importing relevant libraries
 
-q_e = 1.602e-19
-# charge of electron
-q_i = -1.602e-19
-# charge of ion
 
-N_electrons = 500
-N_ions      = 500
+def initial_particles(N_particles, x_wind, y_wind, z_wind, key):
+# this method initializes the velocties and the positions of the particles
+    x = jax.random.uniform(key, shape = (N_particles,), minval=0, maxval=x_wind)
+    y = jax.random.uniform(key, shape = (N_particles,), minval=0, maxval=y_wind)
+    z = jax.random.uniform(key, shape = (N_particles,), minval=0, maxval=z_wind)
+    # initialize the positions of the particles
+    v_x        = jax.numpy.zeros(shape = (N_particles) )
+    v_y        = jax.numpy.zeros(shape = (N_particles) )
+    v_z        = jax.numpy.zeros(shape = (N_particles) )
+    # initialize the velocities of the particles
+    return x, y, z, v_x, v_y, v_z
 
-t_wind = 10e-9
-Nt     = 1000
-dt     = t_wind / Nt
-print(f'time window: {t_wind}')
-print(f'Nt:          {Nt}')
-print(f'dt:          {dt}')
+@jit
+def laplacian(field):
+    x_comp = (jnp.roll(field, shift=1, axis=0) + jnp.roll(field, shift=-1, axis=0) - 2*field)/(dx*dx)
+    y_comp = (jnp.roll(field, shift=1, axis=1) + jnp.roll(field, shift=-1, axis=1) - 2*field)/(dy*dy)
+    z_comp = (jnp.roll(field, shift=1, axis=2) + jnp.roll(field, shift=-1, axis=2) - 2*field)/(dz*dz)
+    return x_comp + y_comp + z_comp
 
-Nx = 1000
-Ny = 1000
-Nz = 1000
-
-x_wind = 0.5
-y_wind = 0.5
-z_wind = 0.5
-
-dx, dy, dz = x_wind/Nx, y_wind/Ny, z_wind/Nz
-
-print(f'Dx: {dx}')
-print(f'Dy: {dy}')
-print(f'Dz: {dz}')
-
-key = random.key(0)
-
-electron_x = jax.random.uniform(key, shape = (N_electrons,), minval=0, maxval=x_wind)
-electron_y = jax.random.uniform(key, shape = (N_electrons,), minval=0, maxval=y_wind)
-electron_z = jax.random.uniform(key, shape = (N_electrons,), minval=0, maxval=z_wind)
-
-ion_x      = jax.random.uniform(key, shape = (N_ions,), minval=0, maxval=x_wind)
-ion_y      = jax.random.uniform(key, shape = (N_ions,), minval=0, maxval=y_wind)
-ion_z      = jax.random.uniform(key, shape = (N_ions,), minval=0, maxval=z_wind)
-
-ev_x        = jax.numpy.zeros(shape = (N_electrons) )
-ev_y        = jax.numpy.zeros(shape = (N_electrons) )
-ev_z        = jax.numpy.zeros(shape = (N_electrons) )
-
-iv_x        = jax.numpy.zeros(shape = (N_ions) )
-iv_y        = jax.numpy.zeros(shape = (N_ions) )
-iv_z        = jax.numpy.zeros(shape = (N_ions) )
-
-
-Ex = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
-Ey = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
-Ez = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
-
-Bx = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
-By = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
-Bz = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
 
 @jit
 def compute_rho(electron_x, electron_y, electron_z, ion_x, ion_y, ion_z):
@@ -91,17 +56,51 @@ def compute_rho(electron_x, electron_y, electron_z, ion_x, ion_y, ion_z):
 
     return rho
 
+def compute_Eforce(q, Ex, Ey, Ez, x, y, z):
+    # This method computes the force from the electric field using interpolation
+    Fx = q*jax.scipy.ndimage.map_coordinates(Ex, [x, y, z], order=1)
+    Fy = q*jax.scipy.ndimage.map_coordinates(Ey, [x, y, z], order=1)
+    Fz = q*jax.scipy.ndimage.map_coordinates(Ez, [x, y, z], order=1)
+    # interpolate the electric field component arrays and calculate the force
+    return Fx, Fy, Fz
 
 
+def update_velocity(vx, vy, vz, Fx, Fy, Fz, dt, m):
+    # update the velocity of the particles
+    vx = vx + (Fx*dt/m)
+    vy = vy + (Fy*dt/m)
+    vz = vz + (Fz*dt/m)
+    return vx, vy, vz
+
+def update_position(x, y, z, vx, vy, vz):
+    # update the position of the particles
+    x = x + vx*dt
+    y = y + vy*dt
+    z = z + vz*dt
+    return x, y, z
+
+
+def plot(x, y, z, t):
+    # makes a 3d plot of the positions of the particles
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(electron_x, electron_y, electron_z)
+    ax.set_xlabel('X (m)')
+    ax.set_ylabel('Y (m)')
+    ax.set_zlabel('Z (m)')
+    plt.title("Particle Positions")
+    plt.savefig(f"particles.{t}.png", dpi=200)
+    ax.cla()
+
+############## DEBUG THIS ###########################################################################
 def delx(field, dx):
-    return (jnp.roll(field.vals, shift=1, axis=0) - jnp.roll(field.vals, shift=-1, axis=0))/(2*dx) 
+    return (jnp.roll(field.vals, shift=1, axis=0) - jnp.roll(field.vals, shift=-1, axis=0))/(2*dx)
 
 def dely(field, dy):
-    return (jnp.roll(field.vals, shift=1, axis=1) - jnp.roll(field.vals, shift=-1, axis=1))/(2*dy) 
+    return (jnp.roll(field.vals, shift=1, axis=1) - jnp.roll(field.vals, shift=-1, axis=1))/(2*dy)
 
 def delz(field, dz):
-    return (jnp.roll(field.vals, shift=1, axis=2) - jnp.roll(field.vals, shift=-1, axis=2))/(2*dz) 
-
+    return (jnp.roll(field.vals, shift=1, axis=2) - jnp.roll(field.vals, shift=-1, axis=2))/(2*dz)
 
 def curl(field_x, field_y, field_z, dx, dy, dz):
     x_comp =  dely(field_z, dy) - delz(field_y, dz)
@@ -109,79 +108,105 @@ def curl(field_x, field_y, field_z, dx, dy, dz):
     z_comp =  delx(field_y, dx) - dely(field_x, dz)
     return x_comp, y_comp, z_comp
 
-
-
 def update_b(Bx, By, Bz, Ex, Ey, Ez, dt, dx, dy, dz):
     dBx, dBy, dBz = -curl(Ex, Ey, Ez, dx, dy, dz) * dt
     return Bx + dBx, By + dBy, Bz + dBz
-    
-@jit
-def laplacian(field):
-    x_comp = (jnp.roll(field, shift=1, axis=0) + jnp.roll(field, shift=-1, axis=0) - 2*field)/(dx*dx)
-    y_comp = (jnp.roll(field, shift=1, axis=1) + jnp.roll(field, shift=-1, axis=1) - 2*field)/(dy*dy)
-    z_comp = (jnp.roll(field, shift=1, axis=2) + jnp.roll(field, shift=-1, axis=2) - 2*field)/(dz*dz)
-    return x_comp + y_comp + z_comp
 
-# @jit
-# def CG_poisson(guess, phi, r):
-#     rprime = jnp.transpose(r)
-#     phiprime = jnp.transpose(phi)
-#     alpha = rprime*r / (phiprime*laplacian(phi))
-#     guess = guess + alpha * phi
-#     rnew = r - rprime*r/phiprime
-#     beta = jnp.transpose(rnew) * rnew / (rprime * r)
-#     r = rnew
-#     phi = r + beta * phi
-#     return guess, phi, r
+##########################################################################################################
 
 
-# def compute_poisson(rho, guess, phi, r, eps):
-#     for i in range(1000):
-#         guess, phi, r = CG_poisson(guess, phi, r)    
-#     return phi
 
-start = time.time()
+
+
+
+
+
+
+
+
+############################ INITIALIZE EVERYTHING #######################################################
+# I am starting by simulating a hydrogen plasma
+print("Initializing Simulation...")
+
+me = 9.1093837e-31
+# mass of the electron
+mi = 1.67e-23
+# mass of the ion
+q_e = 1.602e-19
+# charge of electron
+q_i = -1.602e-19
+# charge of ion
+
+N_electrons = 500
+N_ions      = 500
+# specify the number of electrons and ions in the plasma
+t_wind = 10e-9
+Nt     = 1000
+dt     = t_wind / Nt
+print(f'time window: {t_wind}')
+print(f'Nt:          {Nt}')
+print(f'dt:          {dt}')
+
+Nx = 1000
+Ny = 1000
+Nz = 1000
+# specify the number of array spacings in x, y, and z
+x_wind = 0.5
+y_wind = 0.5
+z_wind = 0.5
+# specify the size of the spatial window in meters
+
+dx, dy, dz = x_wind/Nx, y_wind/Ny, z_wind/Nz
+# compute the spatial resolution
+print(f'Dx: {dx}')
+print(f'Dy: {dy}')
+print(f'Dz: {dz}')
+
+Ex = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
+Ey = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
+Ez = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
+Bx = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
+By = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
+Bz = jax.numpy.zeros(shape = (Nx, Ny, Nz) )
+# initialize the electric and magnetic field arrays as 0
+
+key = random.key(0)
+electron_x, electron_y, electron_z, ev_x, ev_y, ev_z  = initial_particles(N_electrons, x_wind, y_wind, z_wind, key)
+ion_x, ion_y, ion_z, iv_x, iv_y, iv_z                 = initial_particles(N_ions, x_wind, y_wind, z_wind, key)
+# initialize the positions and velocities of the electrons and ions in the plasma.
+# eventually, I need to update the initialization to use a more accurate position and velocity distribution.
+
+
+############### SOLVE E FIELD ######################################################################################
+print("Solving Electric Field...")
 rho    = compute_rho(electron_x, electron_y, electron_z, ion_x, ion_y, ion_z)
-end = time.time()
-print("Calculated Rho")
-print(f'Time Elapsed: {end-start}')
-
-
-start = time.time()
-rho    = compute_rho(electron_x, electron_y, electron_z, ion_x, ion_y, ion_z)
-end = time.time()
-print("After Jit Calculated Rho")
-print(f'Time Elapsed: {end-start}')
-
-
-phi = jax.scipy.sparse.linalg.cg(laplacian, rho, jnp.zeros(shape = (Nx, Ny, Nz) ), maxiter=1000)
+# compute the charge density of the plasma
+phi = jax.scipy.sparse.linalg.cg(laplacian, rho, rho, maxiter=1000)[0]
 # Use conjugated gradients to calculate the electric potential from the charge density
-Ex, Ey, Ez = -1*jnp.gradient(phi)
+E_fields = jnp.gradient(phi)
+Ex       = -1 * E_fields[0]
+Ey       = -1 * E_fields[1]
+Ez       = -1 * E_fields[2]
 # Calculate the E field using the gradient of the potential
 
+############### UPDATE ELECTRONS ##########################################################################################
+print("Updating Electrons...")
+Fx, Fy, Fz = compute_Eforce(q_e, Ex, Ey, Ez, electron_x, electron_y, electron_z)
+# compute the force on the electrons from the electric field
+ev_x, ev_y, ev_z = update_velocity(ev_x, ev_y, ev_z, Fx, Fy, Fz, dt, me)
+# Update the velocties from the electric field
+electron_x, electron_y, electron_z = update_position(electron_x, electron_y, electron_z, ev_x, ev_y, ev_z)
+# Update the positions of the particles
 
 
-# guess = jnp.zeros(shape = (Nx, Ny, Nz) )
-# r = rho
-# phi = r
+############### UPDATE IONS ################################################################################################
+print("Updating Ions...")
+Fx, Fy, Fz = compute_Eforce(q_i, Ex, Ey, Ez, ion_x, ion_y, ion_z)
+# compute the force on the ions from the electric field
+iv_x, iv_y, iv_z = update_velocity(iv_x, iv_y, iv_z, Fx, Fy, Fz, dt, mi)
+# Update the velocities from the electric field
+ion_x, ion_y, ion_z  = update_position(ion_x, ion_y, ion_z, iv_x, iv_y, iv_z)
+# Update the positions of the particles
 
-# start = time.time()
-# guess, phi, r    = CG_poisson(guess, phi, r)
-# end = time.time()
-# print("One CG Pass")
-# print(f'Time Elapsed: {end-start}')
-
-
-# start = time.time()
-# guess, phi, r    = CG_poisson(guess, phi, r)
-# end = time.time()
-# print("After Jit One CG Pass")
-# print(f'Time Elapsed: {end-start}')
-
-
-
-# start = time.time()
-# phi = compute_poisson(rho, guess, phi, r, 1)
-# end = time.time()
-# print("One Full Poisson Pass")
-# print(f"Time Elapsed: {end-start}")
+plot( jnp.concatenate(electron_x, ion_x),  jnp.concatenate(electron_y, ion_y), jnp.concatenate(electron_z, ion_z), 0)
+# plot the particles and save as png file
