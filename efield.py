d2aa6554ea25644dcb414278fe67303f4040a7d4
Christopher Woolford <christopherwoolford@pop-os.localdomain>
2024-09-04 22:02:24 -0500

    PYPIC3D-23 vectorized charge density calculation

--- efield.py
+++ efield.py
@@ -16,31 +16,20 @@ def laplacian(field, dx, dy, dz):
     z_comp = (jnp.roll(field, shift=1, axis=2) + jnp.roll(field, shift=-1, axis=2) - 2*field)/(dz*dz)
     return x_comp + y_comp + z_comp
 
+@jit
+def index_particles(particle, positions, ds):
+    return (positions.at[particle].get() / ds).astype(int)
 
 @jit
-def compute_rho(rho, electron_x, electron_y, electron_z, ion_x, ion_y, ion_z, dx, dy, dz, q_i, q_e):
-    N_electrons = electron_x.shape[0]
-    N_ions      = ion_x.shape[0]
-
-    for electron in range(N_electrons):
-        x = (electron_x.at[electron].get() / dx).astype(int)
-        y = (electron_y.at[electron].get() / dy).astype(int)
-        z = (electron_z.at[electron].get() / dz).astype(int)
-        # I am starting by just rounding for now
-        # Ideally, I would like to partition the charge across array spacings.
-        rho = rho.at[x,y,z].add(q_e)
-
-    for ion in range(N_ions):
-        x = (ion_x.at[ion].get() / dx).astype(int)
-        y = (ion_y.at[ion].get() / dy).astype(int)
-        z = (ion_z.at[ion].get() / dz).astype(int)
-        # I am starting by just rounding for now
-        # Ideally, I would like to partition the charge across array spacings.
-        rho = rho.at[x,y,z].add(q_i)
-
-    rho = rho / (dx*dy*dz)
-    # divide by cell volume
-    return rho
+def update_rho(Nparticles, particlex, particley, particlez, dx, dy, dz, q, rho):
+    def addto_rho(particle, rho):
+        x = index_particles(particle, particlex, dx)
+        y = index_particles(particle, particley, dy)
+        z = index_particles(particle, particlez, dz)
+        rho = rho.at[x, y, z].add( q / (dx*dy*dz) )
+        return rho
+    
+    return jax.lax.fori_loop(0, Nparticles-1, addto_rho, rho )
 
 @jit
 def solve_poisson(rho, eps, dx, dy, dz, phi=None, M = None):
