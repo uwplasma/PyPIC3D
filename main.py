fixed marder correction and spectral derivatives

main.py
@@ -54,7 +54,7 @@ from src.defaults import (
 )
 
 from src.charge_conservation import (
-    current_correction
+    current_correction, marder_correction
 )
 
 from src.model import (
@@ -221,15 +221,15 @@ key5 = random.key(3456)
 # # create the particle species
 
 #################################### Two Stream Instability #####################################################
-# N_particles = particles[0].get_number_of_particles()
-# electron_x, electron_y, electron_z = particles[0].get_position()
-# alternating_ones = (-1)**jnp.array(range(0,N_particles))
-# v0=1.5*2657603.0
-# ev_x = v0*alternating_ones
-# ev_x *= ( 1 + 0.1*jnp.sin(6*jnp.pi * electron_x / x_wind) )
-# ev_y = jnp.zeros(N_particles)
-# ev_z = jnp.zeros(N_particles)
-# particles[0].set_velocity(ev_x, ev_y, ev_z)
+N_particles = particles[0].get_number_of_particles()
+electron_x, electron_y, electron_z = particles[0].get_position()
+alternating_ones = (-1)**jnp.array(range(0,N_particles))
+v0=1.5*2657603.0
+ev_x = v0*alternating_ones
+ev_x *= ( 1 + 0.1*jnp.sin(6*jnp.pi * electron_x / x_wind) )
+ev_y = jnp.zeros(N_particles)
+ev_z = jnp.zeros(N_particles)
+particles[0].set_velocity(ev_x, ev_y, ev_z)
 
 # # add perturbation to the electron velocities
 
@@ -280,25 +280,43 @@ print(f"Thermal Velocity: {jnp.sqrt(2*kb*Te/me)}\n")
 # avg_jz = []
 
 perturbation_period = 20*dt # starting with 5 dt's for now
-velocity_perturbation = 1e9 # m/s
+velocity_perturbation = 0e9 # m/s
 # perturbation velocity
 def perturb_function(t, dt, perturbation_period, velocity_perturbation):
     perturbation = velocity_perturbation * jnp.sin(2 * jnp.pi * t * dt / perturbation_period)
     return perturbation
 # function to calculate the perturbation
 
+Jx = jnp.zeros((Nx, Ny, Nz))
+Jy = jnp.zeros((Nx, Ny, Nz))
+Jz = jnp.zeros((Nx, Ny, Nz))
+
+
+
+#avg_z = []
+p = []
+
+# By = jnp.ones((Nx, Ny, Nz)) * 1
+# vx, vy, vz = particles[0].get_velocity()
+# #vx += perturb_function(t, dt, perturbation_period, velocity_perturbation)
+# vy = jnp.zeros((N_electrons))
+# vz = jnp.zeros((N_electrons))
+# vx = jnp.ones((N_electrons)) * 0.75e6
+# particles[0].set_velocity(vx, vy, vz)
 
 for t in range(Nt):
     print(f'Iteration {t}, Time: {t*dt} s')
-    vx, vy, vz = particles[0].get_velocity()
-    vx += perturb_function(t, dt, perturbation_period, velocity_perturbation)
-    particles[0].set_velocity(vx, vy, vz)
+
+    # vx, vy, vz = particles[0].get_velocity()
+    # vx += perturb_function(t, dt, perturbation_period, velocity_perturbation)
+    # particles[0].set_velocity(vx, vy, vz)
+
     ############### SOLVE E FIELD ############################################################################################
     M = precondition(NN, phi, rho, model)
     # solve for the preconditioner using the neural network
     if electrostatic:
         Ex, Ey, Ez, phi, rho = calculateE(world, particles, rho, eps, phi, M, t, solver, bc, verbose, GPUs)
-        if verbose: print(f"Calculating Electric Field, Max Value: {jnp.max(Ex)}")
+        if verbose: print(f"Calculating Electric Field, Max Value: {jnp.max(jnp.sqrt(Ex**2 + Ey**2 + Ez**2))}")
         # print the maximum value of the electric field
 
     ################ PARTICLE PUSH ########################################################################################
@@ -306,15 +324,15 @@ for t in range(Nt):
         if verbose: print(f'Updating {particles[i].get_name()}')
         particles[i] = particle_push(particles[i], Ex, Ey, Ez, Bx, By, Bz, grid, staggered_grid, dt, GPUs)
         # use boris push for particle velocities
-        if verbose: print(f"Calculating {particles[i].get_name()} Velocities, Max Value: {jnp.max(particles[i].get_velocity()[0])}")
+        if verbose: print(f"Calculating {particles[i].get_name()} Velocities, Mean Value: {jnp.mean(jnp.abs(particles[i].get_velocity()[0]))}")
         particles[i].update_position(dt, x_wind, y_wind, z_wind)
-        if verbose: print(f"Calculating {particles[i].get_name()} Positions, Max Value: {jnp.max(particles[i].get_position()[0])}")
+        if verbose: print(f"Calculating {particles[i].get_name()} Positions, Mean Value: {jnp.mean(jnp.abs(particles[i].get_position()[0]))}")
 
     ################ FIELD UPDATE #######################################################################
     if not electrostatic:
         if solver == "spectral" or solver == "fdtd":
             Jx, Jy, Jz = current_correction(particles, Nx, Ny, Nz)
-        # calculate the corrections for charge conservation
+        # calculate the corrections for charge conservation using villasenor buneamn 1991
         if solver == "spectral":
             Bx, By, Bz = spectralBsolve(grid, staggered_grid, Bx, By, Bz, Ex, Ey, Ez, world, dt)
         elif solver == "fdtd":
@@ -322,7 +340,7 @@ for t in range(Nt):
         elif solver == "autodiff":
             Bx, By, Bz = autodiff_update_B(Bx, By, Bz, Ex, Ey, Ez, dt)
         # update the magnetic field using the curl of the electric field
-        if verbose: print(f"Calculating Magnetic Field, Max Value: {jnp.max(Bx)}")
+        if verbose: print(f"Calculating Magnetic Field, Max Value: {jnp.max(jnp.sqrt(Bx**2 + By**2 + Bz**2))}")
         # print the maximum value of the magnetic field
 
         if solver == "spectral":
@@ -332,9 +350,15 @@ for t in range(Nt):
         elif solver == 'autodiff':
             Ex, Ey, Ez = autodiff_update_E(Ex, Ey, Ez, Bx, By, Bz, dt, C)
         # update the electric field using the curl of the magnetic field
+
     ################## PLOTTING ########################################################################################
 
     if t % plot_freq == 0:
+        #avg_z.append(particles[0].get_position()[2])
+        p0 = 0
+        for particle in particles:
+            p0 += particle.momentum()
+        p.append(p0)
         # avg_jx.append(jnp.mean(Jx))
         # avg_jy.append(jnp.mean(Jy))
         # avg_jz.append(jnp.mean(Jz))
@@ -441,6 +465,9 @@ if plotKE:
 # plot_probe(avg_jz, "Average Jz", "AverageJz")
 # plot the average current density
 
+#plot_probe(avg_z, "Average Z Position", "AverageZ")
+plot_probe(p, "Total Momentum", "TotalMomentum")
+
 if plot_errors:
     plot_probe(div_error_E, "Divergence Error of E Field", f"div_error_E")
     plot_probe(div_error_B, "Divergence Error of B Field", f"div_error_B")
@@ -449,7 +476,9 @@ end = time.time()
 duration = end - start
 # calculate the total simulation time
 
-simulation_stats = {"total_time": duration, "total_iterations": Nt, "time_per_iteration": duration/Nt}
+simulation_stats = {"total_time": duration, "total_iterations": Nt, "time_per_iteration": duration/Nt, "debye_length": debye.item(), \
+    "plasma_frequency": theoretical_freq.item(), 'thermal_velocity': jnp.sqrt(2*kb*Te/me).item()}
+
 dump_parameters_to_toml(simulation_stats, simulation_parameters, plotting_parameters)
 # save the parameters to an output file
 

src/errors.py
@@ -95,5 +95,5 @@ def compute_electric_divergence_error(Ex, Ey, Ez, rho, eps, dx, dy, dz, solver, 
     elif solver == 'autodiff':
         return 0
 
-    divergence_error = jnp.sum(jnp.abs(divE - rho / eps))
+    divergence_error = jnp.mean(jnp.abs(divE - rho / eps))
     return divergence_error

src/rho.py
@@ -34,48 +34,58 @@ def index_particles(particle, positions, ds):
     """
     return (positions.at[particle].get() / ds).astype(int)
 
-@jit
 def particle_weighting(q, x, y, z, rho, dx, dy, dz, x_wind, y_wind, z_wind):
     """
-    Distributes the charge of a particle to the surrounding grid points.
+    Distribute the charge of a particle to the surrounding grid points.
 
     Parameters:
-    q (float): Charge of the particle.
-    x (float): x-coordinate of the particle.
-    y (float): y-coordinate of the particle.
-    z (float): z-coordinate of the particle.
-    rho (ndarray): Charge density array.
-    dx (float): Grid spacing in the x-direction.
-    dy (float): Grid spacing in the y-direction.
-    dz (float): Grid spacing in the z-direction.
-    x_wind (float): Window in the x-direction.
-    y_wind (float): Window in the y-direction.
-    z_wind (float): Window in the z-direction.
+        q (float): Charge of the particle.
+        x (float): x-coordinate of the particle.
+        y (float): y-coordinate of the particle.
+        z (float): z-coordinate of the particle.
+        rho (ndarray): Charge density array.
+        dx (float): Grid spacing in the x-direction.
+        dy (float): Grid spacing in the y-direction.
+        dz (float): Grid spacing in the z-direction.
+        x_wind (float): Window in the x-direction.
+        y_wind (float): Window in the y-direction.
+        z_wind (float): Window in the z-direction.
 
     Returns:
-    ndarray: Updated charge density array.
+        ndarray: Updated charge density array.
     """
+    # Calculate the nearest grid points
+    x0 = jnp.floor((x + x_wind / 2) / dx).astype(int)
+    y0 = jnp.floor((y + y_wind / 2) / dy).astype(int)
+    z0 = jnp.floor((z + z_wind / 2) / dz).astype(int)
 
+    # Calculate the difference between the particle position and the nearest grid point
+    deltax = (x + x_wind / 2) - x0 * dx
+    deltay = (y + y_wind / 2) - y0 * dy
+    deltaz = (z + z_wind / 2) - z0 * dz
 
-    x0, y0, z0 = ((x + x_wind/2)/dx).astype(int), ((y + y_wind/2)/dy).astype(int), ((z + z_wind/2)/dz).astype(int)
-    deltax, deltay, deltaz = (x + x_wind/2) - x0*dx, (y + y_wind/2) - y0*dy, (z + z_wind/2) - z0*dz
-    # calculate the difference between x and its nearest grid point
-    x1, y1, z1 = x0 + 1, y0 + 1, z0 + 1
-    # calculate the index of the next grid point
+    # Calculate the index of the next grid point
+    x1 = x0 + 1
+    y1 = y0 + 1
+    z1 = z0 + 1
 
-    wx = jnp.select( [x0 == 0, deltax == 0, deltax != 0], [0, 0, deltax/( x + x_wind/2 )] )
-    wy = jnp.select( [y0 == 0, deltay == 0, deltay != 0], [0, 0, deltay/( y + y_wind/2 )] )
-    wz = jnp.select( [z0 == 0, deltaz == 0, deltaz != 0], [0, 0, deltaz/( z + z_wind/2 )] )
-    # calculate the weights for the surrounding grid points
+    # Calculate the weights for the surrounding grid points
+    wx = deltax / (x + x_wind/2)
+    wy = deltay / (y + y_wind/2)
+    wz = deltaz / (z + z_wind/2)
 
-    dv = dx*dy*dz
-    # calculate the volume of each grid point
+    # Calculate the volume of each grid point
+    dv = dx * dy * dz
 
-    rho = rho.at[x0, y0, z0].add( (q/dv)*(1 - wx)*(1 - wy)*(1 - wz), mode='drop' )
-    rho = rho.at[x1, y0, z0].add( (q/dv)*wx*(1 - wy)*(1 - wz)      , mode='drop')
-    rho = rho.at[x0, y1, z0].add( (q/dv)*(1 - wx)*wy*(1 - wz)      , mode='drop')
-    rho = rho.at[x0, y0, z1].add( (q/dv)*(1 - wx)*(1 - wy)*wz      , mode='drop')
-    # distribute the charge of the particle to the surrounding grid points
+    # Distribute the charge of the particle to the surrounding grid points
+    rho = rho.at[x0, y0, z0].add((q / dv) * (1 - wx) * (1 - wy) * (1 - wz), mode='drop')
+    rho = rho.at[x1, y0, z0].add((q / dv) * wx * (1 - wy) * (1 - wz), mode='drop')
+    rho = rho.at[x0, y1, z0].add((q / dv) * (1 - wx) * wy * (1 - wz), mode='drop')
+    rho = rho.at[x0, y0, z1].add((q / dv) * (1 - wx) * (1 - wy) * wz, mode='drop')
+    rho = rho.at[x1, y1, z0].add((q / dv) * wx * wy * (1 - wz), mode='drop')
+    rho = rho.at[x1, y0, z1].add((q / dv) * wx * (1 - wy) * wz, mode='drop')
+    rho = rho.at[x0, y1, z1].add((q / dv) * (1 - wx) * wy * wz, mode='drop')
+    rho = rho.at[x1, y1, z1].add((q / dv) * wx * wy * wz, mode='drop')
 
     return rho
 
@@ -115,7 +125,7 @@ def update_rho(Nparticles, particlex, particley, particlez, dx, dy, dz, q, x_win
         z = index_particles(particle, particlez, dz)
         rho = rho.at[x, y, z].add( q / (dx*dy*dz) )
         return rho
-    
+
     return jax.lax.fori_loop(0, Nparticles, addto_rho, rho )
 
 @use_gpu_if_set

src/spectral.py
@@ -13,7 +13,6 @@ import functools
 from functools import partial
 
 from src.utils import interpolate_and_stagger_field, interpolate_field, use_gpu_if_set
-from src.charge_conservation import current_correction
 from src.particle import particle_species
 
 def detect_gibbs_phenomenon(field, dx, dy, dz, threshold=0.1):
@@ -69,7 +68,7 @@ def spectral_poisson_solve(rho, eps, dx, dy, dz):
     # calculate the squared wavenumber
     k2 = jnp.where(k2 == 0, 1e-6, k2)
     # avoid division by zero
-    phi = -jnp.fft.fftn(rho) / (eps*k2)
+    phi = jnp.fft.fftn(rho) / (eps*k2)
     # calculate the Fourier transform of the charge density and divide by the permittivity and squared wavenumber
     phi = jnp.fft.ifftn(phi).real
     # calculate the inverse Fourier transform to obtain the electric potential
@@ -109,7 +108,7 @@ def spectral_divergence(xfield, yfield, zfield, dx, dy, dz):
     zfft = jnp.fft.fftn(zfield)
     # calculate the Fourier transform of the vector field
 
-    div = 1j*kx*xfft + 1j*ky*yfft + 1j*kz*zfft
+    div = (-1j)*kx*xfft + (-1j)*ky*yfft + (-1j)*kz*zfft
     # calculate the divergence of the vector field
 
     return jnp.fft.ifftn(div).real
@@ -145,9 +144,9 @@ def spectral_curl(xfield, yfield, zfield, dx, dy, dz):
     zfft = jnp.fft.fftn(zfield)
     # calculate the Fourier transform of the vector field
 
-    curlx = 1j*kz*yfft - 1j*ky*zfft
-    curly = 1j*kx*zfft - 1j*kz*xfft
-    curlz = 1j*ky*xfft - 1j*kx*yfft
+    curlx = (-1j)*kz*yfft - (-1j)*ky*zfft
+    curly = (-1j)*kx*zfft - (-1j)*kz*xfft
+    curlz = (-1j)*ky*xfft - (-1j)*kx*yfft
     # calculate the curl of the vector field
 
     return jnp.fft.ifftn(curlx).real, jnp.fft.ifftn(curly).real, jnp.fft.ifftn(curlz).real
@@ -176,9 +175,9 @@ def spectral_gradient(field, dx, dy, dz):
     field_fft = jnp.fft.fftn(field)
     # calculate the Fourier transform of the field
 
-    gradx = 1j * kx * field_fft
-    grady = 1j * ky * field_fft
-    gradz = 1j * kz * field_fft
+    gradx = -1j * kx * field_fft
+    grady = -1j * ky * field_fft
+    gradz = -1j * kz * field_fft
     # calculate the gradient in Fourier space
 
     return jnp.fft.ifftn(gradx).real, jnp.fft.ifftn(grady).real, jnp.fft.ifftn(gradz).real
@@ -217,28 +216,6 @@ def spectralBsolve(grid, staggered_grid, Bx, By, Bz, Ex, Ey, Ez, world, dt):
     Bz = Bz - dt/2*curlz
 
     return Bx, By, Bz
-
-def marder_correction(Ex, Ey, Ez, rho, world, eps):
-    """
-    Apply Marder's correction to the electric field components.
-
-    Parameters:
-    Ex (ndarray): Electric field component in the x-direction.
-    Ey (ndarray): Electric field component in the y-direction.
-    Ez (ndarray): Electric field component in the z-direction.
-    rho (ndarray): Charge density.
-    world (dict): Dictionary containing the grid spacing with keys 'dx', 'dy', and 'dz'.
-    eps (float): Permittivity of the medium.
-
-    Returns:
-    tuple: A tuple containing the correction components (Ex, Ey, Ez).
-    """
-    dx = world['dx']
-    dy = world['dy']
-    dz = world['dz']
-    div_E = spectral_divergence(Ex, Ey, Ez, dx, dy, dz)
-    F = div_E + rho/eps
-    return spectral_gradient(F, dx, dy, dz)
 
 
 @jit