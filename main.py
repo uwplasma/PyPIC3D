1c8adcef9f11f69a8b4138199b44091886adaee4
cwoolfo1 <70346478+cwoolfo1@users.noreply.github.com>
2025-01-20 16:16:06 -0600

    added TSI data generation script and added documentation to the readthedocs. PYPIC3D-29  PYPIC3D-27
    Former-commit-id: 7b96897225914a548e3f1ade662c77903bbcc0e0 [formerly 2d8d2aeeb6cacb7d10c7aa4f797afe698ecc504c]
    Former-commit-id: 1afd752bacbdf4173064d4fc93b199b4a242fd3a
    Former-commit-id: 494039ce7c203f120935a4a616e1a9af3cf58ef0

--- main.py
+++ main.py
@@ -79,11 +79,16 @@ loop = partial(time_loop, Ex_ext=Ex_ext, Ey_ext=Ey_ext, Ez_ext=Ez_ext, Bx_ext=Bx
 start = time.time()
 # start the timer
 
+# jax.profiler.start_trace("/tmp/tensorboard")
+
 for t in tqdm(range(Nt)):
     particles, Ex, Ey, Ez, Bx, By, Bz, Jx, Jy, Jz, rho, phi = loop(t, particles, Ex, Ey, Ez, Bx, By, Bz, Jx, Jy, Jz, rho, phi)
     # time loop to update the particles and fields
     plotter(t, particles, Ex, Ey, Ez, Bx, By, Bz, Jx, Jy, Jz, rho, phi, E_grid, B_grid, world, constants, plotting_parameters, simulation_parameters, solver, bc)
     # plot the data
+
+# jax.profiler.stop_trace()
+# # stop the trace
 
 end = time.time()
 # end the timer
