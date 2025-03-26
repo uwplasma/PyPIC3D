fixed issue in rho calculation
Former-commit-id: f40b766513c7f3bb779257413462db73a4787ea8 [formerly 4587ef5208fbae360aab6738def6bd8535d09b83]
Former-commit-id: fe2145551fd6c62fe5b8c6e9f610da5f43fb1d10
Former-commit-id: b979aec1f847033e14e7492bbb1f0280b8fc5b5a

PyPIC3D/fields.py
@@ -120,13 +120,16 @@ def calculateE(world, particles, constants, rho, phi, M, solver, bc):
     dx = world['dx']
     dy = world['dy']
     dz = world['dz']
+    eps = constants['eps']
 
     rho = compute_rho(particles, rho, world)
     # calculate the charge density based on the particle positions
 
     #if_verbose_print(verbose, f"Calculating Charge Density, Max Value: {jnp.max(jnp.abs(rho))}" )
+    # sor = functools.partial(solve_poisson_sor, dx=dx, dy=dy, dz=dz, eps=eps, omega=0.15, tol=1e-12, max_iter=30000)
+    # phi = sor(phi, rho)
 
-    phi = phi.at[:,:,:].set(solve_poisson(rho, constants, world, phi=phi, solver=solver, bc=bc, M=M))
+    phi = solve_poisson(rho, constants, world, phi=phi, solver=solver, bc=bc, M=M)
     # solve the Poisson equation to get the electric potential
 
     #if_verbose_print(verbose, f"Calculating Electric Potential, Max Value: {jnp.max(phi)}",  )