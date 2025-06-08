0027b3b00ee39fcc2a0c2951605e493ec69f15ed
cwoolfo1 <70346478+cwoolfo1@users.noreply.github.com>
2025-06-08 14:26:52 -0500

    account for J=rhov in my electrodynamic evolve loop

--- PyPIC3D/evolve.py
+++ PyPIC3D/evolve.py
@@ -12,7 +12,7 @@ from PyPIC3D.fields import (
 )
 
 from PyPIC3D.J import (
-    VB_correction
+    VB_correction, J_from_rhov
 )
 
 from PyPIC3D.boris import (
@@ -110,6 +110,8 @@ def time_loop_electrodynamic(particles, E, B, J, rho, phi, E_grid, B_grid, world
         # update the particle positions
 
     ################ FIELD UPDATE ################################################################################################
+    J, rho = J_from_rhov(particles, J, rho, constants, world)
+    # calculate the current density from the particle positions and velocities
     J = VB_correction(particles, J, constants)
     # calculate the corrections for charge conservation using villasenor buneamn 1991
     E = update_E(E, B, J, world, constants, curl_func)
