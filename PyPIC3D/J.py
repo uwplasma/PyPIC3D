fixing bug in current calculation

PyPIC3D/J.py
@@ -49,9 +49,9 @@ def J_from_rhov(particles, J, constants, world, grid):
         total_y = jnp.concatenate( [species.get_position()[1] for species in particles] )
         total_z = jnp.concatenate( [species.get_position()[2] for species in particles] )
 
-        total_dqvx = jnp.concatenate( [species.get_charge() * species.get_velocity()[0] for species in particles] )
-        total_dqvy = jnp.concatenate( [species.get_charge() * species.get_velocity()[1] for species in particles] )
-        total_dqvz = jnp.concatenate( [species.get_charge() * species.get_velocity()[2] for species in particles] )
+        total_dqvx = jnp.concatenate( [species.get_charge() / (dx*dy*dz) * species.get_velocity()[0] for species in particles] )
+        total_dqvy = jnp.concatenate( [species.get_charge() / (dx*dy*dz) * species.get_velocity()[1] for species in particles] )
+        total_dqvz = jnp.concatenate( [species.get_charge() / (dx*dy*dz) * species.get_velocity()[2] for species in particles] )
         # concatenate all the particle data for easier processing
 
         shape_factor = particles[0].get_shape()