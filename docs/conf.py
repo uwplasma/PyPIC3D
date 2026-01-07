30a74fe9370bdbca2d810f2b21dfb7c5ae549663
Christopher Woolford <clwoolford@wisc.edu>
2026-01-07 11:05:27 -0600

    updating read the docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,11 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
+    'sphinx_rtd_theme',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
