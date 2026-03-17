f72cd2d7076f88d1bb684d93dd57c4aeec3b5ced
Christopher Woolford <clwoolford@wisc.edu>
2026-03-17 11:34:52 -0500

    fixing file imports

--- PyPIC3D/__init__.py
+++ PyPIC3D/__init__.py
@@ -6,22 +6,28 @@ import os, sys
 import matplotlib.pyplot as plt
 # import external libraries
 
-jax.config.update('jax_platform_name', 'cpu')
-
 from . import errors
 from . import boundaryconditions
 from . import initialization
+from . import utils
+from . import boris
+from . import evolve
+
+from .solvers import vector_potential
+from .solvers import pstd
+from .solvers import fdtd
+from .solvers import first_order_yee
+from .solvers import electrostatic_yee
+
 from .particles import particle_initialization
 from .particles import species_class
+
 from .deposition import shapes
 from .deposition import Esirkepov
 from .deposition import J_from_rhov
 from .deposition import rho
+
 from .diagnostics import plotting
-from . import utils
-from .solvers import pstd
-from .solvers import fdtd
-from . import boris
-from . import rho
-from . import evolve
-from .solvers import vector_potential
+from .diagnostics import fluid_quantities
+from .diagnostics import openPMD
+from .diagnostics import vtk
